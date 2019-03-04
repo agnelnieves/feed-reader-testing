27ddcf20604e62340e0bbd668058a6d137d80e30
Agnel Nieves <agnelnieves@gmail.com>
2019-03-03 19:56:32 -0500

    Feat: Add Url test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -33,6 +33,16 @@ $(function() {
          */
 
 
+        it('url defined', function () {
+            for(let feed of allFeeds){
+               //  Expecting values
+               expect(feed.url).toBeDefined();
+               // Checking length
+               expect(feed.url.length).not.toBe(0);
+            }
+        });
+
+
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
